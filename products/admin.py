1608f890fe30f08de70ffb09061ac4c5155dac30
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-04-18 12:47:02 +0100

    Removed cloudinary save_model and image list display

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from django.conf import settings
-from cloudinary import uploader
 from .models import Product, Category
-
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
@@ -10,20 +7,9 @@ class ProductAdmin(admin.ModelAdmin):
         'name',
         'price',
         'rating',
-        'image',
     )
 
     ordering = ('sku',)
-
-    def save_model(self, request, obj, form, change):
-        # Check if a new image is uploaded
-        if 'image' in request.FILES:
-            # Upload new image to Cloudinary
-            image = request.FILES['image']
-            uploaded_image = uploader.upload(image, folder=settings.CLOUDINARY_FOLDER_NAME)
-            obj.image = uploaded_image.url
-        super().save_model(request, obj, form, change)
-
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
