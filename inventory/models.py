cdf1ed1bbd8e77ca24f7f58ea8dd3fcd634cb84a
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-04-22 13:40:46 +0100

    Add product and addon inventories

--- inventory/models.py
+++ inventory/models.py
@@ -1,2 +1,17 @@
 from django.db import models
+from products.models import Product, AddOn
 
+class Inventory(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name='inventory')
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.product.name} - Quantity: {self.quantity}"
+
+
+class AddOnInventory(models.Model):
+    addon = models.OneToOneField(AddOn, on_delete=models.CASCADE, related_name='inventory')
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.addon.name} - Quantity: {self.quantity}"
