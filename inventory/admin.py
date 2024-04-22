b4cf51ce8c26dca78719cbb69b575b259d09c675
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-04-22 13:39:53 +0100

    Added inventories to admin site

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Inventory, AddOnInventory
 
-# Register your models here.
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity')
+    search_fields = ('product__name',)
+    list_per_page = 20
+
+
+class AddOnInventoryAdmin(admin.ModelAdmin):
+    list_display = ('addon', 'quantity')
+    search_fields = ('addon__name',)
+    list_per_page = 20
+
+admin.site.register(Inventory, InventoryAdmin)
+admin.site.register(AddOnInventory, AddOnInventoryAdmin)
