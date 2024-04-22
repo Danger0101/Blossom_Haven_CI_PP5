8f7f1139d77687240cc9478fe52a3975a15cae1d
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-04-22 13:39:25 +0100

    Added update inventory signals to checkout signals

--- checkout/signals.py
+++ checkout/signals.py
@@ -2,6 +2,9 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from .models import OrderLineItem
+from products.models import Product
+from inventory.models import Inventory
+
 
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
@@ -16,3 +19,28 @@ def update_on_delete(sender, instance, **kwargs):
     Update order total on lineitem delete
     """
     instance.order.update_total()
+    
+
+@receiver(post_delete, sender=OrderLineItem)
+def restore_inventory(sender, instance, **kwargs):
+    """
+    Restore inventory upon order line item deletion
+    """
+    product = instance.product
+    inventory = Inventory.objects.get(product=product)
+    quantity_sold = instance.quantity
+    inventory.quantity += quantity_sold
+    inventory.save()
+
+@receiver(post_save, sender=OrderLineItem)
+def update_inventory(sender, instance, created, **kwargs):
+    """
+    Update inventory upon order line item creation
+    """
+    print('update_inventory signal received!')
+    if created:
+        product = instance.product
+        inventory = Inventory.objects.get(product=product)
+        quantity_sold = instance.quantity
+        inventory.quantity -= quantity_sold
+        inventory.save()
