3de542261904583b0401c615aa9008085bc3c2fd
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-05-03 13:44:32 +0100

    Added ordering and list per page

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Product, Category
 
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'sku',
@@ -8,14 +9,18 @@ class ProductAdmin(admin.ModelAdmin):
         'price',
         'rating',
     )
+    ordering = ('name', 'sku', 'price', 'rating',)
+    search_fields = ('sku', 'name')
+    list_per_page = 20
 
-    ordering = ('sku',)
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
     )
+    search_fields = ('friendly_name', 'name')
+    list_per_page = 20
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
