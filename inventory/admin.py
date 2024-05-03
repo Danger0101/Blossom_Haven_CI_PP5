a76c4fbd2b4460ab964a2249b09c59314763aaa2
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-05-03 13:46:00 +0100

    Removed addon inventory model

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,16 +1,9 @@
 from django.contrib import admin
-from .models import Inventory, AddOnInventory
+from .models import Inventory
 
 class InventoryAdmin(admin.ModelAdmin):
     list_display = ('product', 'quantity')
     search_fields = ('product__name',)
     list_per_page = 20
 
-
-class AddOnInventoryAdmin(admin.ModelAdmin):
-    list_display = ('addon', 'quantity')
-    search_fields = ('addon__name',)
-    list_per_page = 20
-
 admin.site.register(Inventory, InventoryAdmin)
-admin.site.register(AddOnInventory, AddOnInventoryAdmin)
--- inventory/models.py
+++ inventory/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from products.models import Product, AddOn
+from products.models import Product
 
 class Inventory(models.Model):
     product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name='inventory')
@@ -7,11 +7,3 @@ class Inventory(models.Model):
 
     def __str__(self):
         return f"{self.product.name} - Quantity: {self.quantity}"
-
-
-class AddOnInventory(models.Model):
-    addon = models.OneToOneField(AddOn, on_delete=models.CASCADE, related_name='inventory')
-    quantity = models.IntegerField(default=0)
-
-    def __str__(self):
-        return f"{self.addon.name} - Quantity: {self.quantity}"
