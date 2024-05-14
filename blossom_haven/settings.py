7e3599a9fda699a5a8075f118d5d972f3a057ee9
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-05-14 20:38:23 +0100

    Added in email functionality to settings

--- blossom_haven/settings.py
+++ blossom_haven/settings.py
@@ -248,3 +248,12 @@ cloudinary.config(
     api_secret=CLOUDINARY_API_SECRET_KEY
 )
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+# Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASS', '')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
