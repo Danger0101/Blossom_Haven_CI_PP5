9facbe0218bd74ac138adae70ea48a9aec124e38
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-05-14 14:11:39 +0100

    Removed signal as no longer needed

--- reviews/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import Review
-
-@receiver(post_save, sender=Review)
-def update_product_rating(sender, instance, created, **kwargs):
-    if created or instance.user_rating != instance._state.fields_cache['user_rating']:
-        instance.product.rating = instance.product.calculate_average_rating()
-        instance.product.save()
