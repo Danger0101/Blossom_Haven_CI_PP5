c32ec789bfce5c31d47b673b5a449272cb238423
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-05-15 15:33:45 +0100

    Added checkout success email

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
@@ -204,15 +205,8 @@ def send_order_confirmation_email(order):
     Send email notification to user about their order
     """
     subject = 'Order Confirmation'
-    message = f'Hi {order.full_name},\n\nThank you for your order. Your order number is {order.order_number}.\n\n'
-    message += 'Order Details:\n'
-    # Add order details to the email message
-    for item in order.lineitems.all():
-        message += f'- {item.product.name} ({item.quantity})\n'
-    message += f'Total: {order.grand_total}\n\n'
-    message += 'We will update you soon about the delivery status.\n\n'
-    message += 'Thank you for shopping with us!\n\n'
-    message += 'Best regards,\nThe Blossom Haven Team'
-
+    template = 'order_confirmation_email.html'
+    context = {'order': order}
+    message = render_to_string(template, context)
     # Send email using Django's built-in email sending functionality
     send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [order.email])
