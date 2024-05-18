c43bc461be6ddb9617e6871d34f5415b5edd87a6
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-05-18 18:32:03 +0100

    Refactored in a bug squashing attempt and its fixed
    issue was relating to the issue of adding items and stock at the same time with images caused a 500 error and a url error. All is now functioning

--- products/forms.py
+++ products/forms.py
@@ -3,15 +3,12 @@ from .widgets import CustomClearableFileInput
 from .models import Product, Category
 from inventory.models import Inventory
 
-
 class ProductForm(forms.ModelForm):
     quantity = forms.IntegerField(label='Quantity', required=True)
-    is_addon = forms.BooleanField(label='Is Addon', required=False)
 
     class Meta:
         model = Product
         fields = [
-            'sku',
             'category',
             'name',
             'description',
@@ -20,19 +17,18 @@ class ProductForm(forms.ModelForm):
             'image_url',
             'is_addon',
         ]
-
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+        widgets = {
+            'image': CustomClearableFileInput(),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
 
-        # Fetch initial quantity only if the instance has been saved
         if self.instance.pk:
             product_inventory = Inventory.objects.filter(product=self.instance)
             if product_inventory.exists():
@@ -40,16 +36,12 @@ class ProductForm(forms.ModelForm):
             else:
                 initial_quantity = 0
             self.fields['quantity'].initial = initial_quantity
-
-            # Set initial value for is_addon field
             self.fields['is_addon'].initial = self.instance.is_addon
 
     def save(self, commit=True):
-        # Save inventory data along with product
         instance = super().save(commit=False)
         if commit:
             instance.save()
-            # Ensure that the product instance is saved before accessing inventory
             product_inventory, created = Inventory.objects.get_or_create(product=instance)
             product_inventory.quantity = self.cleaned_data['quantity']
             product_inventory.save()
